8138bed48a25259c184e907224fa50c9a9a6b7e8
Luca Piccarreta <luca.piccarreta@foosoft.it>
2024-05-07 01:28:40 +0200

    Experiment with table visualization

--- readxml/QAction_3/QAction_3.cs
+++ readxml/QAction_3/QAction_3.cs
@@ -1,13 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 using Skyline.DataMiner.Scripting;
-using System.Xml.Serialization;
-using Skyline.DataMiner.Scripting;
-using System.Xml;
-using System.IO;
 
 /// <summary>
 /// DataMiner QAction Class.
@@ -20,56 +19,6 @@ public static class QAction
     /// <param name="protocol">Link with SLProtocol process.</param>
 
 
-    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
-    /// <remarks/>
-    [System.SerializableAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
-    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
-    public partial class items
-    {
-
-        private itemsItem[] itemField;
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute("item")]
-        public itemsItem[] item
-        {
-            get
-            {
-                return this.itemField;
-            }
-            set
-            {
-                this.itemField = value;
-            }
-        }
-    }
-
-    /// <remarks/>
-    [System.SerializableAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
-    public partial class itemsItem
-    {
-
-        private byte idField;
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlAttributeAttribute()]
-        public byte id
-        {
-            get
-            {
-                return this.idField;
-            }
-            set
-            {
-                this.idField = value;
-            }
-        }
-    }
-
     public static void Run(SLProtocolExt protocol)
     {
         try
@@ -78,35 +27,40 @@ public static class QAction
             // Get Text based xml data as a string.
             string textData = @"
                 <items>
-                  <item id=""1""/>
-                  <item id=""2""/>
-                  <item id=""3""/>
-                  <item id=""4""/>
-                  <item id=""5""/>
-                  <item id=""6""/>
+                  <item id=""1"" value=""just""/>
+                  <item id=""2"" value=""some""/>
+                  <item id=""3"" value=""random""/>
+                  <item id=""4"" value=""data4""/>
                 </items>
             ";
             // Convert C# Generated Classes into Qaction Rows.
             items items_ = XmlDeserializeFromString<items>(textData);
 
-            List<object[]> instances = new List<object[]>();
             // Convert Generated class into Connector Row data.
-            foreach (itemsItem instance in items_.item)
-            {
-                instances.Add(new TablenameQActionRow
-                {
-                    Tablenameinstance_2001 = instance.id,
-                    Tablenameinstance2_2002 = instance.id,
-                }.ToObjectArray());
-            }
+            var rows = ConvertToDamn(items_);
             protocol.Debug = items_.item.Length;
-            protocol.FillArray(Parameter.Tablename.tablePid, instances, NotifyProtocol.SaveOption.Full);
+            protocol.FillArray(Parameter.Tablename.tablePid, rows, NotifyProtocol.SaveOption.Full);
         }
         catch (Exception ex)
         {
+            protocol.Debug = "Failed parsing xml file"; ;
             protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown:{Environment.NewLine}{ex}", LogType.Error, LogLevel.NoLogging);
         }
     }
+    public static List<object[]> ConvertToDamn(items items_)
+    {
+        List<object[]> instances = new List<object[]>();
+        foreach (itemsItem instance in items_.item)
+        {
+            instances.Add(new TablenameQActionRow
+            {
+                Tablenameinstance_2001 = instance.id,
+                Tablenameinstance2_2002 = instance.value,
+            }.ToObjectArray());
+        }
+        return instances;
+    }
+
     public static T XmlDeserializeFromString<T>(this string xmlTextData)
     {
         object result;
---
+++ readxml/QAction_3/Xml.cs
@@ -0,0 +1,70 @@
+﻿
+// C# Artifacts computed from sample.xml with "Paste special" option
+
+// NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+/// <remarks/>
+
+
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+[System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+public partial class items
+{
+
+    private itemsItem[] itemField;
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlElementAttribute("item")]
+    public itemsItem[] item
+    {
+        get
+        {
+            return this.itemField;
+        }
+        set
+        {
+            this.itemField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+public partial class itemsItem
+{
+
+    private byte idField;
+
+    private string valueField;
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public byte id
+    {
+        get
+        {
+            return this.idField;
+        }
+        set
+        {
+            this.idField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string value
+    {
+        get
+        {
+            return this.valueField;
+        }
+        set
+        {
+            this.valueField = value;
+        }
+    }
+}
