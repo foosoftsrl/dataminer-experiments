4af53594387758b70ef683e1b09d64c9a8851e45
Luca Piccarreta <luca.piccarreta@foosoft.it>
2024-05-06 16:40:22 +0200

    Proof of concept XML parsing

--- readxml/QAction_3/QAction_3.cs
+++ readxml/QAction_3/QAction_3.cs
@@ -4,6 +4,10 @@ using System.Globalization;
 using System.Text;
 
 using Skyline.DataMiner.Scripting;
+using System.Xml.Serialization;
+using Skyline.DataMiner.Scripting;
+using System.Xml;
+using System.IO;
 
 /// <summary>
 /// DataMiner QAction Class.
@@ -14,16 +18,103 @@ public static class QAction
     /// The QAction entry point.
     /// </summary>
     /// <param name="protocol">Link with SLProtocol process.</param>
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class items
+    {
+
+        private itemsItem[] itemField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("item")]
+        public itemsItem[] item
+        {
+            get
+            {
+                return this.itemField;
+            }
+            set
+            {
+                this.itemField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class itemsItem
+    {
+
+        private byte idField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
     public static void Run(SLProtocolExt protocol)
     {
         try
         {
-            protocol.Debug = protocol.Anumber;
             protocol.Anumber = (double)protocol.Anumber + 2;
+            // Get Text based xml data as a string.
+            string textData = @"
+                <items>
+                  <item id=""1""/>
+                  <item id=""2""/>
+                  <item id=""3""/>
+                  <item id=""4""/>
+                  <item id=""5""/>
+                </items>
+            ";
+            // Convert C# Generated Classes into Qaction Rows.
+            List<object[]> instances = new List<object[]>();
+            items items_ = XmlDeserializeFromString<items>(textData);
+
+            foreach (itemsItem instance in items_.item)
+            {
+            }
+            protocol.Debug = items_.item.Length;
+            //protocol.FillArray(Parameter.Instancesxml.tablePid, instances, NotifyProtocol.SaveOption.Full);
         }
         catch (Exception ex)
         {
             protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Exception thrown:{Environment.NewLine}{ex}", LogType.Error, LogLevel.NoLogging);
+        }
+    }
+    public static T XmlDeserializeFromString<T>(this string xmlTextData)
+    {
+        object result;
+        try
+        {
+            using (TextReader reader = new StringReader(xmlTextData))
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                result = serializer.Deserialize(reader);
+            }
+            return (T)result;
+        }
+        catch
+        {
+            return default(T);
         }
     }
 }
